5617bdb783bcd4ebcdb6bd6a8c7bafdb6169fa6e
João Fontebasso <joao.alvescont@gmail.com>
2024-04-07 13:38:41 -0300

    Exercicios iniciais

---
+++ app.py
@@ -0,0 +1,18 @@
+
+############################################
+## REGISTRANDO O NOME E CONTANDO CARACTERES
+############################################
+
+nome = input("Digite seu nome: ")
+tmNome = len(nome)
+print(f'Olá, {nome} o seu nome tem {tmNome} letras!')
+
+
+############################################
+## DIGITAR DOIS VALORES E SOMAR
+############################################
+
+N1 = int(input('Digite o seu primeiro número: '))
+N2 = int(input('Digite o seu primeiro número: '))
+
+print(f'A soma dos seu valores é {N1+N2}')
