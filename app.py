eef014c6386be2112e39568e3e2efe251f004168
João Fontebasso <joao.alvescont@gmail.com>
2024-04-07 13:48:42 -0300

    desafio

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+############################################
+## DESAFIO
+############################################
+
+print('''
+######################################
+INICIANDO PROGRAMA DE CÁLCULO DE BONUS
+######################################
+      ''')
+
+nome2 = input("Digite o seu nome: ")
+sal = float(input("Digite o seu salário $: "))
+bonus = float(input("Digite o seu bônus %: "))
+
+print(f'O seu salário era de {sal} e com o bonus de {bonus}%, passou a ser {sal + (sal * (bonus/100))}')
+
+
 
 ############################################
 ## REGISTRANDO O NOME E CONTANDO CARACTERES
@@ -15,4 +32,6 @@ print(f'Olá, {nome} o seu nome tem {tmNome} letras!')
 N1 = int(input('Digite o seu primeiro número: '))
 N2 = int(input('Digite o seu primeiro número: '))
 
-print(f'A soma dos seu valores é {N1+N2}')
+print(f'A soma dos seu valores é {N1+N2}') 
+
+
